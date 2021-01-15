cb4712312fcf412291573ad1fb9672f472d2b37f
limisie <k.limisiewicz@gmail.com>
2021-01-15 01:53:22 +0100

    Add participant model

--- buzkashi_app/models.py
+++ buzkashi_app/models.py
@@ -22,36 +22,55 @@ class Competition(models.Model):
         # sesja dla szkoły średniej
         HIGH_SCHOOL_SESSION = 2
 
+    # nazwa: VARCHAR(50), NOT NULL, UNIQUE
+    title = models.CharField(max_length=50, unique=True)
+
+    # sesja: INTEGER, NOT NULL
+    session = models.IntegerField(choices=Session.choices, default=Session.UNIVERSITY_SESSION)
+
     # data rozpoczęcia: TIMESTAMP, NOT NULL, DEFAULT=NOW
     start_date = models.DateTimeField(default=timezone.now)
 
     # data zakończenia: TIMESTAMP, NOT NULL, DEFAULT=NOW
-    completion_date = models.DateTimeField(default=timezone.now)
+    end_date = models.DateTimeField(default=timezone.now)
 
     # czas trwania: FLOAT, NOT NULL
     duration = models.FloatField()
-
-    # sesja: INTEGER, NOT NULL
-    session = models.IntegerField(choices=Session.choices, default=Session.UNIVERSITY_SESSION)
+    #TODO: django ma typ DurationField
 
     # maksymalna liczba zespołów: INTEGER, NOT NULL
     max_teams = models.IntegerField(verbose_name='Max Number Of Teams')
 
     # czy zawody testowe: BIT, NOT NULL
-    test_run = models.BooleanField()
+    is_test = models.BooleanField()
 
-    # opis: VARCHAR(255)
+    # opis: VARCHAR(255) TODO: chyba opis powinien być dłuższy
     description = models.CharField(max_length=255, blank=True, null=True)
 
     # TODO: tutaj powinny być klucze obce do rankingu, ale nie wiadomo czy będzię on przechowywany w bazie
 
 
-class EduInstitute(models.Model):
+class EduInstitution(models.Model):
     """
     Klasa ORM placówki edukacyjnej
     id generowane automatycznie
     """
-    pass
+
+    # nazwa: VARCHAR(50), NOT NULL, UNIQUE
+    name = models.CharField(max_length=50, unique=True)
+
+    # wojewodztwo: VARCHAR(50), NOT NULL, UNIQUE
+    region = models.CharField(max_length=50, unique=True)
+    # TODO: enumerator? chyba nie
+
+    # email: VARCHAR(50), NOT NULL, UNIQUE
+    email = models.CharField(max_length=50, unique=True)
+
+    # kod autoryzacji: VARCHAR(50), NOT NULL
+    authorization_code = models.CharField(max_length=50)
+
+    # czy wyższa: BIT, NOT NULL
+    is_university = models.BooleanField(default=True)
 
 
 class Team(models.Model):
@@ -65,13 +84,9 @@ class Team(models.Model):
     name = models.CharField(max_length=50, unique=True)
 
     # ocena: INTEGER, NOT NULL
-    score = models.IntegerField()
-
-    # czy zakwalifikowany: BIT, NOT NULL
-    qualified = models.BooleanField()
-
-    # czy zdyskwalifikowany: BIT, NOT NULL
-    disqualified = models.BooleanField()
+    score = models.IntegerField(default=0)
+    # TODO: ocena to suma czasu rozwiązywania zadań,
+    #      więc chyba też mogłóby być to pole typu Duration
 
     # data zgłoszenia: TIMESTAMP, NOT NULL, DEFAULT=NOW
     application_date = models.DateTimeField(default=timezone.now())
@@ -79,8 +94,14 @@ class Team(models.Model):
     # priorytet: INTEGER, NOT NULL DEFAULT=1
     priority = models.IntegerField(default=1)
 
+    # czy zakwalifikowany: BIT, NOT NULL
+    is_qualified = models.BooleanField(default=False)
+
+    # czy zdyskwalifikowany: BIT, NOT NULL
+    is_disqualified = models.BooleanField(default=False)
+
     # opiekun: VARCHAR(255)
-    guardian = models.CharField(max_length=255, blank=True, null=True)
+    tutor = models.CharField(max_length=255, blank=True, null=True)
 
     # powiązane konto
     associated_user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -89,9 +110,31 @@ class Team(models.Model):
     competition = models.ForeignKey(Competition, on_delete=models.CASCADE)
 
     # placówka edukacyjna: FOREIGN KEY(EduInstitute)
-    institute = models.ForeignKey(EduInstitute, on_delete=models.PROTECT)
+    institution = models.ForeignKey(EduInstitution, on_delete=models.PROTECT)
 
     # TODO: tutaj powinien być klucz obcy do rankingu, ale nie wiadomo czy będzię on przechowywany w bazie
+    # TODO: czy w zespole jest potrzebny ranking? Zespół mógłby odczytywać ranking przez Zawody
+
+
+class Participant(models.Model):
+    """
+    Klasa ORM uczestnika
+    id generowane automatycznie
+    """
+
+    # imię: VARCHAR(50), NOT NULL
+    name = models.CharField(max_length=50)
+
+    # nazwisko: VARCHAR(50), NOT NULL
+    surname = models.CharField(max_length=50)
+    # TODO: czy potrzebne są imiona, nazwiska i emaile
+    #       jak uzytkownik auth je ma?
+
+    # czy kapitan: BIT, NOT NULL
+    is_capitan = models.BooleanField(default=False)
+
+    # zespół: FOREIGN KEY(Team)
+    team = models.ForeignKey(Team, on_delete=models.CASCADE)
 
 
 class Judge(models.Model):
@@ -108,7 +151,7 @@ class Judge(models.Model):
     surname = models.CharField(max_length=50)
 
     # czy główny: BIT, NOT NULL
-    chief = models.BooleanField()
+    is_chief = models.BooleanField()
 
     # powiązane konto
     associated_user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -120,11 +163,13 @@ class Task(models.Model):
     id generowane automatycznie
     """
 
-    # nazwa: VARCHAR(255), NOT NULL
-    name = models.CharField(max_length=255)
+    # tytuł: VARCHAR(255), NOT NULL
+    title = models.CharField(max_length=255)
 
     # treść: VARCHAR(500), NOT NULL
-    content = models.CharField(max_length=500)
+    body = models.CharField(max_length=500)
+    # TODO: chyba wystarczy pole z treścią, nie potrzeba dodatkowo opisu?
+    #       i też powinno być dłuższe
 
     # opis: VARCHAR(255)
     description = models.CharField(max_length=255, blank=True, null=True)
@@ -133,10 +178,10 @@ class Task(models.Model):
     author = models.ForeignKey(Judge, on_delete=models.CASCADE)
 
     # zawody: FOREIGN KEY(Competition)
-    competition = models.ForeignKey(Competition, on_delete=models.PROTECT)
+    competition = models.ForeignKey(Competition, default=None, on_delete=models.PROTECT)
 
 
-class AutomaticTest(models.Model):
+class AutomatedTest(models.Model):
     """
     Klasa ORM testu automatycznego
     id generowane automatycznie
@@ -144,7 +189,7 @@ class AutomaticTest(models.Model):
     pass
 
 
-class AutomaticTestResult(models.Model):
+class AutomatedTestResult(models.Model):
     """
     Klasa ORM wyniku testu automatycznego
     id generowane automatycznie
@@ -185,5 +230,18 @@ class Solution(models.Model):
         # rozwiązanie odrzucone
         REJECTED = 'Odrzucone'
 
+    # author = models.ForeignKey(Team)
+    #
+    # task = models.ForeignKey(Task)
+    #
+    # version = models.IntegerField(default=1)
+    #
+    # time = models.FloatField()
+    #
+    # score = models.DurationField()
+    # TODO: czy potrzebujemy osobnych pól na czas i ocenę?
+
     # kod źródłowy: FILE SAVED IN DIRECTORY NOT DATABASE!
     source_code = models.FileField()
+
+
