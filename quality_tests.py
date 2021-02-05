64d69a0c4c7895159e7a7bced2b628ecfaad5b3a
limisie <k.limisiewicz@gmail.com>
2021-02-05 23:44:43 +0100

    Delete locust task: task_create_page

--- quality_tests.py
+++ quality_tests.py
@@ -1,6 +1,3 @@
-import random
-import string
-
 from locust import task, HttpUser, between
 
 USERNAME = 'limi'
@@ -28,10 +25,6 @@ class GuestUser(HttpUser):
 
 
 class JudgeUser(HttpUser):
-    """
-    Zestaw testów jakościowych dla żądań HTTP sędziego.
-    """
-
     wait_time = between(1, 5)
 
     def on_start(self):
@@ -55,14 +48,6 @@ class JudgeUser(HttpUser):
     @task
     def task_create_page(self):
         self.client.get(url='/tasks/create/')
-
-    # @task
-    # def task_create_page(self):
-    #     response = self.client.get("/tasks/create/")
-    #     csrftoken = response.cookies['csrftoken']
-    #     self.client.post(url='/tasks/create/',
-    #                      json={'title': ''.join(random.choices(string.ascii_letters, k=5)), 'body': 'Treść'},
-    #                      headers={'X-CSRFToken': csrftoken})
 
     @task
     def task_edit_page(self):
